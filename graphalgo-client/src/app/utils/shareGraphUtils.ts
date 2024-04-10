c49269fb168fe6ae4ede5afc276509c5bd3cd43f
NadiiaNanivska <nanivskan14@gmail.com>
2024-04-10 19:12:50 +0300

    implemented feature of sharing graph

---
+++ graphalgo-client/src/app/utils/shareGraphUtils.ts
@@ -0,0 +1,56 @@
+import SockJS from "sockjs-client";
+import Stomp from 'stompjs';
+import { Data, data } from "./data";
+import { message } from "antd";
+
+export const sendGraph = (user: string, currentUser: string, data: Data) => {
+    const res = JSON.stringify(data);
+    console.log(res)
+    const socket = new SockJS('http://localhost:8080/ws');
+    const stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        if (user !== currentUser) {
+            stompClient.send('/chat.invite', {}, JSON.stringify({ senderId: currentUser, recipientId: user, data: res }));
+        }
+    }
+    );
+    setTimeout(() => {
+        stompClient.disconnect(() => { message.warning("Connection closed") });
+    }, 30000);
+}
+
+export const receiveGraph = async (currentUser: string, connectionEstablished: boolean, setConnectionEstablished: React.Dispatch<React.SetStateAction<boolean>>): Promise<Data | null> => {
+    const socket = new SockJS('http://localhost:8080/ws');
+    const stompClient = Stomp.over(socket);
+    const connectPromise = new Promise<void>((resolve, reject) => {
+        stompClient.connect({}, function (frame) {
+            resolve();
+        }, function (error) {
+            reject(error);
+        });
+    });
+    await connectPromise;
+    return new Promise<Data | null>((resolve, reject) => {
+        if (!connectionEstablished) {
+            setConnectionEstablished(true);
+            stompClient.subscribe('/topic/public/' + currentUser, function (msg) {
+                const chatroomId: Data = JSON.parse(msg.body);
+                const nodes = chatroomId.nodes.map(node => {
+                    return { id: node.id };
+                });
+                const links = chatroomId.links.map(link => {
+                    const { source, target, weight } = link;
+                    return { source: (source as any).id, target: (target as any).id, weight };
+                });
+                const sanitizedData = {
+                    nodes: nodes,
+                    links: links
+                };
+                resolve(sanitizedData);
+            });
+            setTimeout(() => {
+                stompClient.disconnect(() => { setConnectionEstablished(false); message.warning("Waiting time is over. Click on receive button again!") });
+            }, 30000);
+        }
+    });
+}
--- graphalgo-client/src/components/GraphTools/GraphTools.tsx
+++ graphalgo-client/src/components/GraphTools/GraphTools.tsx
@@ -1,12 +1,11 @@
 import { InfoCircleOutlined, PlayCircleOutlined, SettingOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Menu, Modal } from 'antd';
-import React from 'react';
-
-import './GraphTools.css';
-import { useData, useGraphOptions } from '../../contexts/GraphOptionsContext';
-import { Data, Link } from '../../app/utils/data';
+import React, { useState } from 'react';
+import { receiveGraph, sendGraph } from '../../app/utils/shareGraphUtils';
 import { downloadTxtFile, generateAdjacencyMatrix, generateIncidenceMatrix, handleAdjacencyMatrixFromFile, handleIncidenceMatrixFromFile } from '../../app/utils/utilFunctions';
+import { useData, useGraphOptions } from '../../contexts/GraphOptionsContext';
+import './GraphTools.css';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -39,15 +38,34 @@ const items: MenuProps['items'] = [
         getItem('Incidence matrix', '9')
     ]),
 
-    getItem('Graph algorithms', 'sub4', <PlayCircleOutlined />, [
+    getItem('Graph algorithms', 'sub3', <PlayCircleOutlined />, [
         getItem('Shortest path algorithms', 'g1', null, [getItem('Dijkstra\'s algorithm', '10'), getItem('Floyd\'s algorithm', '11')], 'group'),
+    ]),
+
+    getItem('Share graph', 'sub4', <InfoCircleOutlined />, [
+        getItem('Send', '12'),
+        getItem('Receive', '13')
     ]),
 ];
 
 const GraphTools = () => {
     console.log("rendered menu")
+    const [connectionEstablished, setConnectionEstablished] = useState<boolean>(false);
     const { setCanAddNode, setCanAddEdge, setCanRemoveEdge, setCanRemoveNode } = useGraphOptions();
     const { nodes, links, setNodes, setLinks } = useData();
+
+    const fetchData = async () => {
+        receiveGraph(window.location.href, connectionEstablished, setConnectionEstablished)
+            .then(data => {
+                if (data !== null) {
+                    setNodes(data.nodes);
+                    setLinks(data.links);
+                }
+            })
+            .catch(error => {
+                console.error('Error receiving graph:', error);
+            });
+    };
 
     const onClick: MenuProps['onClick'] = (e: any) => {
         const key = e.key.toString();
@@ -74,7 +92,7 @@ const GraphTools = () => {
                         <input type="file" accept="text/plain" onChange={(e) => handleAdjacencyMatrixFromFile(e, setNodes, setLinks)} />
                     </div>
                 ),
-                okButtonProps: { style: { backgroundColor: '#FD744F', borderColor: '#fcbdac' }},
+                okButtonProps: { style: { backgroundColor: '#FD744F', borderColor: '#fcbdac' } },
                 cancelButtonProps: { style: { backgroundColor: 'white', borderColor: '#fcbdac', color: 'black' } },
             });
         } else if (key === '7') {
@@ -85,7 +103,7 @@ const GraphTools = () => {
                         <input type="file" accept="text/plain" onChange={(e) => handleIncidenceMatrixFromFile(e, setNodes, setLinks)} />
                     </div>
                 ),
-                okButtonProps: { style: { backgroundColor: '#FD744F', borderColor: '#fcbdac' }},
+                okButtonProps: { style: { backgroundColor: '#FD744F', borderColor: '#fcbdac' } },
                 cancelButtonProps: { style: { backgroundColor: 'white', borderColor: '#fcbdac', color: 'black' } },
             });
         } else if (key === '8') {
@@ -94,6 +112,10 @@ const GraphTools = () => {
         } else if (key === '9') {
             const content = generateIncidenceMatrix(nodes, links).map(row => row.join(' ')).join('\n');
             downloadTxtFile('graphalgo-incidence-matrix.txt', content);
+        } else if (key === '12') {
+            sendGraph("http://localhost:3001/calculator", window.location.href, {nodes, links});
+        } else if (key === '13') {
+            fetchData();
         }
     };
 

61d2e6dc583205fa60bbec7cebc5aeb1ec26c3f0
NadiiaNanivska <nanivskan14@gmail.com>
2024-05-01 13:04:24 +0300

    minor changes

--- graphalgo-server/src/main/java/com/example/graphalgoserver/dto/graph/ShortestPathResponse.java
+++ graphalgo-server/src/main/java/com/example/graphalgoserver/dto/graph/ShortestPathResponse.java
@@ -1,19 +1,17 @@
 package com.example.graphalgoserver.dto.graph;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @NoArgsConstructor
 public class ShortestPathResponse extends VerticesResponse {
-    public List<Double> edges;
+    public List<Double> edgesCost;
     public Double cost;
 
     public ShortestPathResponse(List<String> vertexList, List<Double> list, double weight) {
         super(vertexList);
-        edges = list;
+        edgesCost = list;
         cost = weight;
     }
 }
